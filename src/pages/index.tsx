46d96942585852781ebd7ed5b6b275dbe4be9e60
sajasmohammed <sajasam97@gmail.com>
2024-04-03 15:33:02 +0530

    intial

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home({data}:any) {
+  
+  return (
+    <div className="listItem">
+      <h1>Data Fetching Using Static Site Generation</h1>
+
+      <ul>
+        {
+          data?.map((item:any, index:number)=>(
+            <li key={index}>Product Name: {item.product}</li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps=async ()=>{
+
+  const data=await fetch('https://660d0e4e3a0766e85dbf6ade.mockapi.io/api/testitem/products');
+  const posts= await data.json()
+
+  return {
+    props:{
+     data: posts
+    },
+    revalidate:60
+  }
+}
